76db11a650ace4d992e6625f8eeabcab00a25d63
peter j <info@peterjamack.com>
2018-07-28 18:53:39 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+# remove exited containers:
+sudo docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+    
+# remove unused images:
+sudo docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+
+# stragglers
+sudo docker image prune -a
+sudo docker container prune --filter "until=72h"
+sudo docker volume prune
+sudo docker network prune --filter "until=72h"
+
+#spin up your docker images and run streamiot app
+#sudo docker build --no-cache -t streamiot_mvp_1
+sudo docker-compose down --remove orphans
+
+# enter fednode direcotory
+cd federatednode
+
+#
+sudo ln -sf `pwd`/fednode.py /usr/local/bin/fednode
+
+#start up streamiot fednode full master
+fednode install full master
+
