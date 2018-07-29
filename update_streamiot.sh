945841d461f627092ee7aa0984d68c12a9e8ce33
peter j <info@peterjamack.com>
2018-07-29 19:55:54 -0400

    Update update_streamiot.sh

--- update_streamiot.sh
+++ update_streamiot.sh
@@ -1,9 +1,9 @@
 
 
-docker-compose ps # lists all services (id, name)
-docker-compose stop <id/name> #this will stop only the selected container
-docker-compose rm <id/name> # this will remove the docker container permanently 
-docker-compose up # builds/rebuilds all not already built container 
+###docker-compose ps # lists all services (id, name)
+#33docker-compose stop <id/name> #this will stop only the selected container
+###docker-compose rm <id/name> # this will remove the docker container permanently 
+###docker-compose up # builds/rebuilds all not already built container 
 
 # start up the streamiot node
 sudo docker-compose -f docker-compose.streamiot.yml  up --build
